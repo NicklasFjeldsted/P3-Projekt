9ef530ba13d64525553c49ce85d5af41810af8f6
Alex <31203252+Alex-Eriksen@users.noreply.github.com>
2022-02-10 07:52:01 +0100

    I fixed some stuff

--- TEC-KasinoAPI/SQLQueries/data_snippet_alexander.sql
+++ TEC-KasinoAPI/SQLQueries/data_snippet_alexander.sql
@@ -6,7 +6,7 @@ GO
 INSERT INTO AccountGenders (GenderName) VALUES('Male'),('Female');
 GO
 INSERT INTO Customers (Email, Password, CountryID, PhoneNumber, CPRNumber, FirstName, LastName, Address, ZipCodeID, GenderID)
-	VALUES('alexanderv.eriksen@gmail.com', HASHBYTES('SHA2_256', 'Passw0rd'), SCOPE_IDENTITY(), 40306827, 230801, 'Alexander', 'Eriksen', 'Auderød Byvej 12', 3300, SCOPE_IDENTITY());
+	VALUES('alexanderv.eriksen@gmail.com', HASHBYTES('SHA2_256', 'Passw0rd'), 1, 40306827, 230801, 'Alexander', 'Eriksen', 'Auderød Byvej 12', 3300, 1);
 GO
-INSERT INTO AccountBalances (CustomerID, Balance) VALUES(SCOPE_IDENTITY(), 100000);
+INSERT INTO AccountBalances (CustomerID, Balance, DepositLimit) VALUES(SCOPE_IDENTITY(), 100000, 100);
 GO
--- TEC-KasinoAPI/SQLQueries/drop_create.bat
+++ TEC-KasinoAPI/SQLQueries/drop_create.bat
@@ -2,4 +2,4 @@ sqlcmd -S DESKTOP-SRN9QO0 -i dropdatabase.sql -Usa -P1234
 pause
 cd ../
 dotnet ef database update
-pause
+cd SQLQueries
