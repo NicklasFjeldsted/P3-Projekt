dc07c2b02e8fdfb2ff4c320c59c14648e95f043a
Alex <31203252+Alex-Eriksen@users.noreply.github.com>
2022-02-10 10:17:20 +0100

    Update stored procedures

--- TEC-KasinoAPI/SQLQueries/StoredProcedures/sp_save_all.sql
+++ TEC-KasinoAPI/SQLQueries/StoredProcedures/sp_save_all.sql
@@ -4,10 +4,13 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+IF OBJECT_ID('sp_save_all', 'P') IS NOT NULL
+	DROP PROC sp_save_all
+GO
 CREATE PROCEDURE sp_save_all
 	@CustomerID INT,
 	@Email NVARCHAR(450),
-	@CountryID INT,
+	@CountryName NVARCHAR(MAX),
 	@PhoneNumber INT,
 	@CPRNumber NVARCHAR(11),
 	@FirstName NVARCHAR(MAX),
@@ -20,8 +23,12 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
+	DECLARE @TempCountryID INT;
+
+	SELECT @TempCountryID = CountryID FROM Countries WHERE CountryName = @CountryName;
+
 	UPDATE Customers
-	SET Email = @Email, CountryID = @CountryID, PhoneNumber = @PhoneNumber,
+	SET Email = @Email, CountryID = @TempCountryID, PhoneNumber = @PhoneNumber,
 	CPRNumber = @CPRNumber, FirstName = @FirstName, LastName = @LastName,
 	Address = @Address, ZipCodeID = @ZipCodeID, GenderID = @GenderID
 	WHERE CustomerID = @CustomerID;
--- TEC-KasinoAPI/SQLQueries/StoredProcedures/sp_save_deposit_limit.sql
+++ TEC-KasinoAPI/SQLQueries/StoredProcedures/sp_save_deposit_limit.sql
@@ -4,6 +4,9 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+IF OBJECT_ID('sp_save_deposit_limit', 'P') IS NOT NULL
+	DROP PROC sp_save_deposit_limit
+GO
 CREATE PROCEDURE sp_save_deposit_limit
 	@NewLimit INT,
 	@BalanceID INT
