4ee0ae344f97d17c268dc40ea41604ff3100b9d9
Alex <31203252+Alex-Eriksen@users.noreply.github.com>
2022-02-11 10:38:03 +0100

    Automatic Stored Procedure Init & Changes to Email Search Stored Procedure

--- SQLQueries/StoredProcedures/sp_email_search.sql
+++ SQLQueries/StoredProcedures/sp_email_search.sql
@@ -4,6 +4,9 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+IF OBJECT_ID('sp_email_search', 'P') IS NOT NULL
+	DROP PROC sp_email_search
+GO
 CREATE PROCEDURE sp_email_search
 	@Email nvarchar(450)
 AS
--- SQLQueries/stored_procedure_init.bat
+++ SQLQueries/stored_procedure_init.bat
@@ -1,5 +1,2 @@
-sqlcmd -S DESKTOP-SRN9QO0 -i StoredProcedures\sp_balance_change.sql -Usa -P1234
-sqlcmd -S DESKTOP-SRN9QO0 -i StoredProcedures\sp_email_search.sql -Usa -P1234
-sqlcmd -S DESKTOP-SRN9QO0 -i StoredProcedures\sp_save_deposit_limit.sql -Usa -P1234
-sqlcmd -S DESKTOP-SRN9QO0 -i StoredProcedures\sp_save_all.sql -Usa -P1234
-sqlcmd -S DESKTOP-SRN9QO0 -i StoredProcedures\sp_create_customer.sql -Usa -P1234
+@echo off
+FOR %%x in (StoredProcedures\sp*.sql) DO sqlcmd -S DESKTOP-SRN9QO0 -i  %%x -Usa -P1234
