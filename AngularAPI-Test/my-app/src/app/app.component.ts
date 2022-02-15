157b5b7a0c3420c7b1eefb1265fab9361fc0ef43
Matthias Skov Bryde <matthiasskovbryde@gmail.com>
2022-02-15 10:02:11 +0100

    Update app.component.ts

--- AngularAPI-Test/my-app/src/app/app.component.ts
+++ AngularAPI-Test/my-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ApiService } from './api.service';
+import { ApiService } from './services/api-service/api.service';
 
 @Component({
   selector: 'app-root',
