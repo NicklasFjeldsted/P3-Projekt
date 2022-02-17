20c3a5624f73ea6698d3e9ec4ddcb96f417edfc8
Matthias Skov Bryde <matthiasskovbryde@gmail.com>
2022-02-17 10:23:48 +0100

    Update CSS & HTML

--- Website/Kasino/src/app/app-routing.module.ts
+++ Website/Kasino/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndbetalComponent } from './components/indbetal/indbetal.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent },
+  {path: 'indbetal', component: IndbetalComponent }
 ];
 
 @NgModule({
--- Website/Kasino/src/app/app.module.ts
+++ Website/Kasino/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
+import { IndbetalComponent } from './components/indbetal/indbetal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    IndbetalComponent
   ],
   imports: [
     BrowserModule,
