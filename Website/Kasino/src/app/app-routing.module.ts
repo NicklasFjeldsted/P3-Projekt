ae5a622ad48ed22e5f37b17075da04650352fa2a
Alex <31203252+Alex-Eriksen@users.noreply.github.com>
2022-02-17 10:10:28 +0100

    A lot

--- Website/Kasino/src/app/app-routing.module.ts
+++ Website/Kasino/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  {path: '', component: LoginComponent}
 ];
 
 @NgModule({
--- Website/Kasino/src/app/app.module.ts
+++ Website/Kasino/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
