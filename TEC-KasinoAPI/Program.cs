880301c2c3dba146af971be2a182f1ffa8a2157f
Alex <31203252+Alex-Eriksen@users.noreply.github.com>
2022-02-25 08:50:54 +0100

    Comments

--- TEC-KasinoAPI/Program.cs
+++ TEC-KasinoAPI/Program.cs
@@ -7,7 +7,6 @@ using TEC_KasinoAPI.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using Newtonsoft.Json.Serialization;
 using Swashbuckle.AspNetCore.Filters;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -26,25 +25,39 @@ builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.De
 // Add the AutoMapper to the services of the web application
 builder.Services.AddAutoMapper(typeof(Program));
 
+#region Get the AppSettings model & configure it to the web application
 // Set the configurations of the web application
 IConfigurationSection appSettingsSection = builder.Configuration.GetSection("AppSettings");
 builder.Services.Configure<AppSettings>(appSettingsSection);
+#endregion
 
+#region Encrypting the master key
+/// Encrypt the master key I.E. <see cref="AppSettings.Secret"/>
+AppSettings appSettings = appSettingsSection.Get<AppSettings>();
+byte[] key = Encoding.ASCII.GetBytes(appSettings.Secret);
+#endregion
+
+// Exposes the API to the web application
 builder.Services.AddEndpointsApiExplorer();
 
-// Something about sharing the data within the web application
+#region Add and configure CORS
+// CORS (Cross-Origin Resource Sharing) allows or denies access from frontend JavaScript.
+// CORS is used to create security around what JavaScript is allowed to access the web application.
 builder.Services.AddCors(options =>
 {
-	// Not sure, will return to comment later (maybe)
+	// Defines the policy for frontend JavaScript.
 	options.AddPolicy("EnableCORS", builder =>
 	{
+		// Currently any frontend JavaScript is allowed.
 		builder.AllowAnyOrigin()
 		.AllowAnyHeader()
 		.AllowAnyMethod();
 	});
 });
+#endregion
 
 #region Add swagger to the web application and configure it
+// Jwt configurations for swagger
 builder.Services.AddSwaggerGen(options => {
 	options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
 	{
@@ -55,20 +68,8 @@ builder.Services.AddSwaggerGen(options => {
 	});
 
 	options.OperationFilter<SecurityRequirementsOperationFilter>(); 
-}); // Jwt configurations for swagger
+});
 #endregion
-
-// JSON Serializer
-builder.Services.AddControllersWithViews()
-	.AddNewtonsoftJson(options =>
-	options.SerializerSettings.ReferenceLoopHandling = Newtonsoft
-	.Json.ReferenceLoopHandling.Ignore)
-	.AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
-	= new DefaultContractResolver());
-
-/// Encrypt the master key I.E. <see cref="AppSettings.Secret"/>
-AppSettings appSettings = appSettingsSection.Get<AppSettings>();
-byte[] key = Encoding.ASCII.GetBytes(appSettings.Secret);
 
 #region Add authentication to the web application and define how it should operate
 builder.Services.AddAuthentication(x =>
@@ -91,42 +92,43 @@ builder.Services.AddAuthentication(x =>
 });
 #endregion
 
-// Not sure, but its something about adding the IUserService & UserService linked with each other
+// Adds the interfaces and classes in a scoped fashion, meaning they will both be destroyed together
+// they share a lifetime together for the full duration of a request. (I think)
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<IBalanceService, BalanceService>();
 
 // Build the web application
 WebApplication app = builder.Build();
 
-// Start swagger & its UI
+// Enable swagger and its UI
 app.UseSwagger();
 app.UseSwaggerUI();
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	//app.UseSwagger();
-	//app.UseSwaggerUI();
-}
-
+// Enable HTTPS redirection for the web application.
 app.UseHttpsRedirection();
 
-// Not sure, something about child-/sub domains
+// Enable CORS and something about allowing things again.
 app.UseCors(x => x
 	.SetIsOriginAllowed(origin => true)
 	.AllowAnyMethod()
 	.AllowAnyHeader()
 	.AllowCredentials());
 
+// Eneables the use of static files for the current directory path.
 app.UseStaticFiles();
 
+// Enables routing from within the backend.
 app.UseRouting();
 
+// Enable authentication for the web application.
 app.UseAuthentication();
 
+// Enables API authortization capabilities.
 app.UseAuthorization();
 
+// Set up the endpoints for the API using the controllers.
 app.UseEndpoints(x => x.MapControllers());
 
+// Run the web application.
 app.Run();
