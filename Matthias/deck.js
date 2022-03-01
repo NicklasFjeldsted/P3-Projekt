747d8bf381bdacec4cffecd1fb3f6df8738184ca
Matthias Skov Bryde <matthiasskovbryde@gmail.com>
2022-03-01 12:33:10 +0100

    asd

--- Matthias/deck.js
+++ Matthias/deck.js
@@ -1,19 +1,40 @@
-const SUITS = ["♥", "♠", "♦", "♣"]
-const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+]
 
-
-export default class Deck{
-  constructor(cards = freshDeck()){
+export default class Deck {
+  constructor(cards = freshDeck()) {
     this.cards = cards
   }
 
-  get numberOfCards(){
+  get numberOfCards() {
     return this.cards.length
   }
 
-  shuffle(){
-    for (let i = this.numberOfCards - 1; i > 0; i--){
-      const newIndex = Math.floor(Math.random() * (i +1))
+  pop() {
+    return this.cards.shift()
+  }
+
+  push(card) {
+    this.cards.push(card)
+  }
+
+  shuffle() {
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * (i + 1))
       const oldValue = this.cards[newIndex]
       this.cards[newIndex] = this.cards[i]
       this.cards[i] = oldValue
@@ -21,18 +42,18 @@ export default class Deck{
   }
 }
 
-class Card{
-  constructor(suit, value){
+class Card {
+  constructor(suit, value) {
     this.suit = suit
     this.value = value
   }
 
-  get color(){
-    return this.suit === '♣' || this.suit === '♠' ? 'black' : 'red' 
+  get color() {
+    return this.suit === "♣" || this.suit === "♠" ? "black" : "red"
   }
 
   getHTML() {
-    const cardDiv = document.createElement('div')
+    const cardDiv = document.createElement("div")
     cardDiv.innerText = this.suit
     cardDiv.classList.add("card", this.color)
     cardDiv.dataset.value = `${this.value} ${this.suit}`
@@ -40,7 +61,7 @@ class Card{
   }
 }
 
-function freshDeck(){
+function freshDeck() {
   return SUITS.flatMap(suit => {
     return VALUES.map(value => {
       return new Card(suit, value)
