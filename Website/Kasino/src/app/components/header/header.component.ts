fd3991bcbdc80c69b4a539340830148ad1078278
NicklasFjeldsted <85939080+NicklasFjeldsted@users.noreply.github.com>
2022-03-14 08:30:46 +0100

    Fixed

--- Website/Kasino/src/app/components/header/header.component.ts
+++ Website/Kasino/src/app/components/header/header.component.ts
@@ -3,9 +3,6 @@ import { MatSidenav } from '@angular/material/sidenav';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
-  host: {
-    '(document:click)': 'onclick($event)',
-  },
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
