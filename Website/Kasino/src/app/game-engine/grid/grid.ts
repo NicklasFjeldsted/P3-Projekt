547a66f15f2d483ecc9a889380ee36f24b309107
Alex <31203252+Alex-Eriksen@users.noreply.github.com>
2022-03-15 14:49:07 +0100

    Update grid.ts

--- Website/Kasino/src/app/game-engine/grid/grid.ts
+++ Website/Kasino/src/app/game-engine/grid/grid.ts
@@ -1,3 +1,12 @@
 import { Entity } from "../utils";
+import { Node } from "../node";
 
-export class Grid extends Entity {  }
+export class Grid extends Entity
+{ 
+	private _nodes: Node[] = [];
+
+	public get Nodes(): Node[]
+	{
+		return this._nodes;
+	}
+}
