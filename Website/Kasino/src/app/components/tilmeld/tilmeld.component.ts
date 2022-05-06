1879770be143c367c66943da16f3ae8dde75264a
NicklasFjeldsted <85939080+NicklasFjeldsted@users.noreply.github.com>
2022-05-06 13:35:00 +0200

    tilmeld code adjustments

--- Website/Kasino/src/app/components/tilmeld/tilmeld.component.ts
+++ Website/Kasino/src/app/components/tilmeld/tilmeld.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import { AbstractControl, FormControl, FormGroup, Validators, FormBuilder } from "@angular/forms";
+import { Component } from "@angular/core";
+import { AbstractControl, FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { CustomerService } from "src/app/services/customer.service";
 import { CustomerRegisterRequest } from "src/app/interfaces/CustomerRegisterRequest";
@@ -11,106 +11,59 @@ import { Country } from "src/app/interfaces/country";
   templateUrl: "./tilmeld.component.html",
   styleUrls: ["./tilmeld.component.css"],
 })
-export class TilmeldComponent implements OnInit {
-  // Creates form
+export class TilmeldComponent {
+  // Creates form with controls
   form: FormGroup = new FormGroup({
-    email: new FormControl(""),
-    password: new FormControl(""),
-    countryID: new FormControl(""),
-    phoneNumber: new FormControl(),
-    cprNumber: new FormControl(),
-    firstName: new FormControl(""),
-    lastName: new FormControl(""),
-    address: new FormControl(""),
-    zipCodeID: new FormControl(),
-    genderID: new FormControl(),
-    acceptTerms: new FormControl(false),
+    email: new FormControl("", [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl("", [Validators.required, Validators.minLength(8)]),
+    countryID: new FormControl("", Validators.required),
+    phoneNumber: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.maxLength(9)]),
+    cprNumber: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
+    firstName: new FormControl("", Validators.required),
+    lastName: new FormControl("", Validators.required),
+    address: new FormControl("", Validators.required),
+    genderID: new FormControl(null, Validators.required),
+    zipCodeID: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(4)]),
+    acceptTerms: new FormControl(false, Validators.requiredTrue),
   });
 
-  step: any = 2; // Current step on form
+  step: any = 1; // Current step on form
   nextSubmit: boolean = false; // checks if client has pressed next on form
-  acceptRights: boolean = false; // checks if client has accepted their rights
   submitted: boolean = false; // checks if client has submitted form
   custom: CustomerRegisterRequest; // customer request model
-  validGender: boolean;
 
-  constructor(
-    private customerService: CustomerService,
-    private router: Router,
-    private authenticationService: AuthenticationService,
-    private formBuilder: FormBuilder
-  ) {}
+  constructor(private customerService: CustomerService, private router: Router, private authenticationService: AuthenticationService) {}
 
-  // Gives form properties validators
-  ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      email: ["", [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-      password: ["", [Validators.required, Validators.minLength(8)]],
-      countryID: ["", Validators.required],
-      phoneNumber: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(9)]],
-      cprNumber: [null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
-      firstName: ["", Validators.required],
-      lastName: ["", Validators.required],
-      address: ["", Validators.required],
-      genderID: [null, Validators.required],
-      zipCodeID: [null, [Validators.required, Validators.minLength(4), Validators.maxLength(4)]],
-      acceptTerms: [false, Validators.requiredTrue],
-    });
-  }
-
+  // Gets all controls from Form
   get f(): { [key: string]: AbstractControl } {
     return this.form.controls;
   }
 
+  // Goes to next step in registration
   goNext(): void {
     this.nextSubmit = true;
-    var countryValue = this.form.get("countryID")?.value;
-    if (this.convertCountry(countryValue) == -1) {
-      return;
-    }
+    if (this.convertCountry(this.f["countryID"].value) == -1) return;
+
+    this.form.patchValue({ countryID: this.convertCountry(this.f["countryID"].value) });
+
     if (this.f["email"].invalid || this.f["password"].invalid || this.f["countryID"].invalid || this.f["phoneNumber"].invalid) return;
-    else {
-      this.form.patchValue({ countryID: this.convertCountry(countryValue) });
-      this.step += 1;
-    }
+
+    this.step += 1;
   }
 
-  convertCountry(countryName: string): number {
-    const countries: Country[] = JSON.parse(localStorage.getItem("countries")!);
-    const country: Country | undefined = countries.find((country) => country.countryName == countryName);
-    if (country === undefined) {
-      console.error("Country not found!");
-      return -1;
-    } else {
-      return country.countryID;
-    }
-  }
-
+  // Registration of customer account
   signUp(): void {
     this.submitted = true;
 
-    this.checkGender() ? (this.validGender = false) : (this.validGender = true);
-
-    if (
-      this.f["cprNumber"].invalid ||
-      this.f["firstName"].invalid ||
-      this.f["lastName"].invalid ||
-      this.f["address"].invalid ||
-      this.f["zipCodeID"].invalid ||
-      this.f["acceptTerms"].invalid
-    )
-      return;
+    if (this.form.invalid) return;
 
     this.custom = Object.assign(this.form.value);
     this.customerService.register(this.custom).subscribe({
       next: () => {
-        let credentials = { email: "", password: "" };
-        credentials = Object.assign(credentials, this.form.value);
-        this.authenticationService.login(credentials.email, credentials.password).subscribe({
+        this.authenticationService.login(this.f["email"].value, this.f["password"].value).subscribe({
           next: () => {
             this.router.navigate([""]);
           },
-
           error: (error) => {
             console.error(error);
             this.router.navigate(["login"]);
@@ -123,10 +76,21 @@ export class TilmeldComponent implements OnInit {
     });
   }
 
-  checkGender(): number | undefined {
-    return this.form.get("genderID")?.value;
+  // Converts country name to associated id
+  convertCountry(countryName: string): number {
+    const countries: Country[] = JSON.parse(localStorage.getItem("countries")!);
+    const country: Country | undefined = countries.find((country) => country.countryName == countryName);
+    if (country === undefined) {
+      console.error("Country not found!");
+      return -1;
+    }
+    return country.countryID;
   }
 
+  // Checks whether customer has assigned a value to genderID
+  checkGender = () => (this.form.get("genderID")?.value === null ? false : true);
+
+  // Sets the id of the gender
   setGender(genderid: Number): void {
     this.form.patchValue({ genderID: genderid });
     const buttonMale = document.getElementById("gender-button-0")!.children;
