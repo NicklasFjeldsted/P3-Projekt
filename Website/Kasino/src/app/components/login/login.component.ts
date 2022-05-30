221178d30c15d5c9eef2cecc212016ce3b318ece
NicklasFjeldsted <85939080+NicklasFjeldsted@users.noreply.github.com>
2022-05-30 11:54:33 +0200

    login compoennt css overhaul + responsive

--- Website/Kasino/src/app/components/login/login.component.ts
+++ Website/Kasino/src/app/components/login/login.component.ts
@@ -1,65 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { first } from 'rxjs';
-import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, NgForm, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { first } from "rxjs";
+import { AuthenticationService } from "src/app/services/authentication.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: [ './login.component.css' ]
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
-export class LoginComponent implements OnInit
-{
-  constructor(private router: Router, private authenticationService: AuthenticationService, private route: ActivatedRoute, private formBuilder: FormBuilder) { }
+export class LoginComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {}
 
   form: FormGroup;
-  loading = false;
   submitted = false;
   returnUrl: string;
-  error = '';
+  show: boolean = false;
 
-  ngOnInit(): void
-  {
+  ngOnInit(): void {
     this.form = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
+      email: ["", Validators.required],
+      password: ["", Validators.required],
     });
-    this.returnUrl = this.route.snapshot.queryParams[ 'returnUrl' ] || '/';
-    this.authenticationService.OnTokenChanged.subscribe((token) =>
-    {
-      if (token !== '')
-      {
-        this.router.navigate([ this.returnUrl ]);
+    this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
+    this.authenticationService.OnTokenChanged.subscribe((token) => {
+      if (token !== "") {
+        this.router.navigate([this.returnUrl]);
       }
     });
   }
 
-  invalidLogin: boolean = false;
+  get f() {
+    return this.form.controls;
+  }
 
-  get f() { return this.form.controls; }
-
-  public onSubmit(): void
-  {
-    this.submitted = true;
-    if(this.form.invalid) 
-    {
-      return;
-    }
-    this.loading = true;
-    this.authenticationService.login(this.f["email"].value, this.f["password"].value)
+  public onSubmit(): void {
+    this.authenticationService
+      .login(this.f["email"].value, this.f["password"].value)
       .pipe(first())
       .subscribe({
-        next: () =>
-        {
-          this.router.navigate([ this.returnUrl ]);
+        next: () => {
+          this.router.navigate([this.returnUrl]);
         },
-        error: error =>
-        {
-          this.error = error;
-          this.loading = false;
-          window.alert("Fejl ved login. Forkert E-mail eller adgangskode");
-        }
+        error: () => {
+          this.submitted = true;
+          this.form.markAsUntouched();
+        },
       });
   }
+
+  togglePassword = () => (this.show = !this.show);
 }
