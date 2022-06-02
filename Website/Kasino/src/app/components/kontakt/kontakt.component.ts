bef3be5f63d2d84e0ee0f064da9c0d963398fb08
NicklasFjeldsted <85939080+NicklasFjeldsted@users.noreply.github.com>
2022-06-02 13:37:01 +0200

    kontakt responsive

--- Website/Kasino/src/app/components/kontakt/kontakt.component.ts
+++ Website/Kasino/src/app/components/kontakt/kontakt.component.ts
@@ -1,45 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { CustomerService } from 'src/app/services/customer.service';
-import { ContactMail } from 'src/app/interfaces/ContactMail';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { CustomerService } from "src/app/services/customer.service";
+import { ContactMail } from "src/app/interfaces/ContactMail";
 
 @Component({
-  selector: 'app-kontakt',
-  templateUrl: './kontakt.component.html',
-  styleUrls: ['./kontakt.component.css']
+  selector: "app-kontakt",
+  templateUrl: "./kontakt.component.html",
+  styleUrls: ["./kontakt.component.css"],
 })
-export class KontaktComponent implements OnInit 
-{
+export class KontaktComponent implements OnInit {
   mail: ContactMail;
   form: FormGroup;
-  mailSent: boolean = false;
 
-  constructor(private builder: FormBuilder, private customerService: CustomerService) { }
+  constructor(private builder: FormBuilder, private customerService: CustomerService) {}
 
-  ngOnInit(): void 
-  {
+  ngOnInit(): void {
     this.form = this.builder.group({
-      Fullname: new FormControl('', [Validators.required]),
-      Email: new FormControl('', [Validators.required, Validators.email,Validators.pattern('^[A-Za-z-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$')]),
-      Subject: new FormControl('', Validators.required),
-      Message: new FormControl('', Validators.required)
-    })
+      Fullname: new FormControl("", [Validators.required]),
+      Email: new FormControl("", [Validators.required, Validators.email, Validators.pattern("^[A-Za-z-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$")]),
+      Subject: new FormControl("", Validators.required),
+      Message: new FormControl("", Validators.required),
+    });
+    this.mail = { Fullname: "", Email: "", Subject: "", Message: "" };
   }
 
-  onSubmit() 
-  {
-    this.mail = {Fullname: '', Email: '', Subject: '', Message: ''};
+  onSubmit() {
     this.mail = Object.assign(this.mail, this.form.value);
     this.customerService.sendMail(this.mail).subscribe({
-      next: () => 
-      {
-        this.mailSent = true;
-        window.alert("Beskeden er sendt!");
+      next: () => {
+        console.log("Success: Contact form has been sent!");
       },
-      error: (error) => 
-      {
-        console.log(error);
-      }
-    })
+      error: (error) => {
+        console.log("Failed: ", error);
+      },
+    });
   }
 }
