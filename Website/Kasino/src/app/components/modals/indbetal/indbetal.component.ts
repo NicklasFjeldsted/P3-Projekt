1a3370ad28c6b4b4386828b434fb8ac71cac0d2b
NicklasFjeldsted <85939080+NicklasFjeldsted@users.noreply.github.com>
2022-06-02 12:41:00 +0200

    css + card info

--- Website/Kasino/src/app/components/modals/indbetal/indbetal.component.ts
+++ Website/Kasino/src/app/components/modals/indbetal/indbetal.component.ts
@@ -58,10 +58,10 @@ export class IndbetalComponent implements OnInit, OnDestroy {
 
     // Gives form validators
     this.form = this.builder.group({
-      cardName: ["", Validators.required],
-      cardNumber: ["", [Validators.required, Validators.minLength(19), Validators.maxLength(19)]],
-      expDate: ["", [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
-      cvv: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
+      cardName: ["Bo Nielsen", Validators.required],
+      cardNumber: ["5497-7453-2611-4515", [Validators.required, Validators.minLength(19), Validators.maxLength(19)]],
+      expDate: ["12/24", [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      cvv: [123, [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
       amount: [null, Validators.required],
     });
     this.updateAmount(100, 0);
