c274bd3f80e1627ea6f9937381048d3d20a306df
NicklasFjeldsted <85939080+NicklasFjeldsted@users.noreply.github.com>
2022-06-29 13:09:05 +0200

    sidebar svg

--- Website/Documentation/src/app/components/article/article.component.ts
+++ Website/Documentation/src/app/components/article/article.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import hljs from 'highlight.js';
 import { Article } from '../../interfaces';
 import articlesData from '../../../assets/data/articles.json';
+import sidebarContent from '../../../assets/data/sidebar-content.json';
 
 @Component({
   selector: 'app-article',
@@ -14,6 +15,7 @@ export class ArticleComponent implements OnInit {
 	constructor(private route: ActivatedRoute, private router: Router) { }
 
 	public article!: Article;
+	public categoryArticles: string[] = [];
 
 	ngOnInit(): void
 	{
@@ -30,12 +32,30 @@ export class ArticleComponent implements OnInit {
 	public load_data(title: string): void
 	{
 		this.article = articlesData.filter(x => x.title == title)[ 0 ];
+
+		this.categoryArticles = [];
+
+		for(const pair of Object.entries(sidebarContent)) {
+			if(pair[0] != this.article.category) {
+				continue;
+			}
+			for(const value of Object.values(pair[1])) {
+				this.categoryArticles.push(value);
+			}
+		}
+		this.currentArticleIndex = this.categoryArticles.indexOf(this.article.title);
 		setTimeout(() => hljs.highlightAll(), 10);
 	}
 
 	public get_date(): Date
 	{
 		return new Date(this.article.date);
+	}
+
+	public currentArticleIndex: number = 0;
+	public navigate(direction: number): void {
+		this.currentArticleIndex += direction;
+		this.router.navigate(['/article'], {queryParams: { title: this.categoryArticles[this.currentArticleIndex]}})
 	}
 }
 
